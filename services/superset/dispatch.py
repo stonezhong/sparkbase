fbe5a434f2bbe57660f0afc59d83d76efaad8b97
Stone Zhong <stonezhong@hotmail.com>
2022-11-08 14:56:21 -0800

    adding superset

--- services/superset/dispatch.py
+++ services/superset/dispatch.py
@@ -5,19 +5,7 @@ import argparse
 import os
 from mordor import prepare_for_docker, AppEnv
 
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='Dispatch Tool'
-    )
-    parser.add_argument(
-        "action", type=str, help="Specify action",
-        choices=['on_stage'],
-        nargs=1
-    )
-    args = parser.parse_args()
-    action = args.action[0]
-
+def on_stage(app_env):
     app_env = AppEnv("superset")
     context = app_env.get_config("_deployment.json")
     context.update({
@@ -31,6 +19,29 @@ def main():
     os.makedirs(os.path.join(app_env.data_dir, "state"), exist_ok=True)
 
     os.chmod(os.path.join(app_env.data_dir, "state"), 0O777)
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Dispatch Tool'
+    )
+    parser.add_argument(
+        "action", type=str, help="Specify action",
+        choices=['on_stage', 'build', 'up'],
+        nargs=1
+    )
+    args = parser.parse_args()
+    action = args.action[0]
+
+    app_env = AppEnv("superset")
+
+    if action == "on_stage":
+        on_stage(app_env)
+    elif action == "up":
+        os.chdir(os.path.join(app_env.data_dir, "docker"))
+        os.system("docker compose up -d")
+    elif action == "build":
+        os.chdir(os.path.join(app_env.data_dir, "docker"))
+        os.system("docker compose build")
 
 if __name__ == '__main__':
     main()
